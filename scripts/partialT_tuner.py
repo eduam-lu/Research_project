7c967d163ee094091587933326be4b73343487dc
Edu <eduardo@mailer>
2025-04-14 15:12:03 +0200

    partialT_tuner.py w MPNN folder parsing and AF3

--- scripts/partialT_tuner.py
+++ scripts/partialT_tuner.py
@@ -18,18 +18,44 @@ from pathlib import Path
 import pandas as pd
 import numpy as np
 import subprocess
+import re
+import json
 
 #%%Functions
-def parse_MPNN_file ():
-    # Given an MPNN formatted file it parses it sequence by sequence, storing in a list a tuple of sequence and score
-    
-    return
-def parse_MPNN_folder():
+def parse_MPNN_file (file):
+    # Given an MPNN formatted file it parses it sequence by sequence, stores each sequence in a list and each score in another list
+    final_scores = []
+    final_sequences = []
+    with open(file, "r") as faa_file :
+        for line in faa_file:
+            if line.startswith(">"):
+                match = re.search(r"score=([\d.]+)", line)
+                if match:
+                    score = float(match.group(1))
+                    final_scores.append(score)
+            else:
+                final_sequences.append(line.strip())
+    return final_sequences,final_scores
+
+def best_seq_extractor (file, seq_info, score_info):
+    # Given a file name,  a list with all the sequences and a list with all the scores. It returns a list with the sequence with the lowest score and the score
+    index_of_lowest = score_info.index(min(score_info))
+    return [str(file), seq_info[index_of_lowest], score_info[index_of_lowest]]
+
+def parse_MPNN_folder(folder):
     # Given a folder with MPNN output files, it takes each file and
     # a. stores all the scores and seqs in a list
     # b. find the tuple with the minimum score and appends it to the global list
     # c. returns a list with all the best sequences and a list with the scores
-    return
+    input_folder = Path(folder)
+    best_sequences = list()
+    for file in input_folder.iterdir():
+        if file.is_file():
+            extracted_seqs,extracted_scores = parse_MPNN_file(file)
+            best_seq_info = best_seq_extractor(file,extracted_seqs,extracted_scores)
+            best_sequences.append(best_seq_info)            
+            
+    return best_sequences
 
 def extract_partialT_df ():
     # Given a data frame it takes the first two digits of the columnn 'file_ID' and stores them in a new columm 'partial_T'
@@ -56,6 +82,7 @@ sample = Path(args.sample)
 Path("partial_T_tuning/rf_outputs").mkdir(parents=True, exist_ok=True)
 Path("partial_T_tuning/MPNN_outputs").mkdir(parents=True, exist_ok=True)
 Path("partial_T_tuning/AF3_outputs").mkdir(parents=True, exist_ok=True)
+Path("partial_T_tuning/AF3_outputs/json_inputs").mkdir(parents=True, exist_ok=True)
 # Store the samples in a dataframe
 sample_df = pd.read_csv(sample)
 
@@ -81,7 +108,7 @@ for T in partial_Ts:
         
         # Join manually to preserve quotes
         full_command = " ".join(rf_command)
-#        subprocess.run(full_command, shell=True)
+        #subprocess.run(full_command, shell=True)
 # Move all the pdbs to a single folder
 # Define source and destination folders
 src_folder = Path("./partial_T_tuning/rf_outputs")
@@ -110,18 +137,62 @@ json_command = (
 #subprocess.run(json_command, shell=True)
 
 MPNN_command = (
-    "conda run -n SE3nv python ~/ProteinMPNN/protein_mpnn_run.py --num_seq_per_target=20 --batch_size=10 --out_folder=" #ask about batch_size
+    "python ~/ProteinMPNN/protein_mpnn_run.py --num_seq_per_target=20 --batch_size=10 --out_folder=" #ask about batch_size
     + MPNN_output_path
-    + " --use_soluble_model --sampling_temp 0.1 --jsonl_path="
+    + " --use_soluble_model --sampling_temp 0.1 --save_scores --save_probs MPPN_probs --jsonl_path="
     + rf_pdb_folder
     + "/test.jsonl" # ask about sampling 
 )
 
-subprocess.run(MPNN_command, shell=True)
+#subprocess.run(MPNN_command, shell=True)
 
 # Parse the MPNN output to select the sequence with the least score and append the sequence and the score to a list
+sequences = parse_MPNN_folder("partial_T_tuning/MPNN_outputs/seqs") # Contains a list of lists with the format [[file, sequence, score], ...]
 
-# Generate alphafold predictions with the sequence list
+### Generate alphafold predictions with the sequence list
+# Set up variables
+
+output_dir = "partial_T_tuning/AF3_outputs"
+env_path = "/home/ingemar/anaconda3/envs/alphafold3"
+json_path = Path("partial_T_tuning/AF3_outputs/json_inputs")
+
+# Generate json inputs for each sequence
+    
+for sequence_info in sequences:
+    file_name = Path(sequence_info[0]).stem
+    seq = sequence_info[1]
+    json_data = {
+        "name": file_name,
+        "sequences": [
+            {
+                "protein": {
+                    "id": "A",
+                    "sequence": seq
+                }
+            }
+        ],
+        "modelSeeds": [1],
+        "dialect": "alphafold3",
+        "version": 1
+    }
+    
+#    with open(json_path / f"{file_name}.json", "w") as f:
+ #       json.dump(json_data, f, indent=2)
+
+# Parse the json files and generate predictions
+
+
+for file in json_path.iterdir():
+    af_command = (
+    f"conda run -p {env_path} python /mnt/data/alphafold3/run_alphafold.py "
+    f"--json_path={file} "
+    f"--output_dir={output_dir} "
+    f"--db_dir=/mnt/data/alphafold3/alphafold_databases/ "
+    f"--model_dir=/mnt/data/alphafold3/models/"
+    )
+    af_process = subprocess.run(af_command, shell = True, text=True)
+
+# Parse pLDDT
 
 # RMSD with the original structure file
 
